f59d4e59b122c84aae50a5f60a634699dc06e369
nicson <ohde.eulab@gmail.com>
2015-06-21 22:25:54 +0900

    Initial Commit

---
+++ BBI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BBI
+//
+//  Created by Yoshiyuki Ohde on 2015/06/21.
+//  Copyright (c) 2015年 Yoshiyuki Ohde. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
